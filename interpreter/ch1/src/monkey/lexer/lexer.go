65475f2a9f6e264012dd365e04bdae529da095e1
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-02 20:02:31 -0500

    feat: update struct variable definition

--- interpreter/ch1/src/monkey/lexer/lexer.go
+++ interpreter/ch1/src/monkey/lexer/lexer.go
@@ -3,10 +3,11 @@ package lexer
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
-	readPosition int  // current reading position in input (after current char)
+	readPosition int  // current reading position in input (points to NEXT char after current)
 	ch           byte // current char under examination
 }
 
+// Returns Lexer for input string
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
