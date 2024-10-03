0f96e5eba2ac9f1271a98ecab4feb3b3cad91aac
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-02 21:53:16 -0500

    fix: correct input

--- interpreter/ch1/src/monkey/lexer/lexer_test.go
+++ interpreter/ch1/src/monkey/lexer/lexer_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=_(){}`
+	input := `=+(){},;`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -30,7 +30,7 @@ func TestNextToken(t *testing.T) {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got = %q", i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
