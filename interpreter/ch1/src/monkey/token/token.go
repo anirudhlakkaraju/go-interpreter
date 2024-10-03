959c5a312e4998816a3126d65951912e5f39f028
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-02 19:56:56 -0500

    feat: type and literal definitions

---
+++ interpreter/ch1/src/monkey/token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // Identifier string for - add, foo, bar, x, y, ...
+	INT   = "INT"   // 12345
+
+	// Operaters
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimieters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
