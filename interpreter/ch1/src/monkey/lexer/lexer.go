2080a2b2ffb717371e2929a9bd0f7064787a6d15
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-02 22:07:28 -0500

    feat: add struct methods to read an identifier string in Lexer input

--- interpreter/ch1/src/monkey/lexer/lexer.go
+++ interpreter/ch1/src/monkey/lexer/lexer.go
@@ -51,12 +51,34 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
 }
 
+// Returns new token
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// Returns identifier string starting from l.position
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// Returns true if ch is Letter
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
