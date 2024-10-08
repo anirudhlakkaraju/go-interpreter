7b6999f0b9472df25541251404831dbc8fb9d314
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-07 22:29:46 -0500

    feat: implement parser to parse let statements

--- interpreter/ch2/src/monkey/parser/parser.go
+++ interpreter/ch2/src/monkey/parser/parser.go
@@ -13,10 +13,11 @@ type Parser struct {
 	peekToken token.Token
 }
 
+// Initializes parser
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 
-	// Read tow tokens so curToken and peekToken are both set
+	// Read two tokens so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
@@ -28,6 +29,68 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+// Actual Parser Implementation
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expression until we encounter semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// Returns true if current token is t
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// Returns true is next token is t
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// Returns true is
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
 }
