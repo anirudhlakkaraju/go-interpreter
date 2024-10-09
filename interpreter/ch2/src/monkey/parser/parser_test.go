12492d70ebdad013110daf5272443661197c6b2c
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-08 22:11:51 -0500

    feat: check for any parser errors

--- interpreter/ch2/src/monkey/parser/parser_test.go
+++ interpreter/ch2/src/monkey/parser/parser_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-	let x = 5;
+	let x 5;
 	let y = 10;
-	let foobar = 838383;
+	let foobar  838383;
 	`
 
 	// Initialize new Lexer and Parser
@@ -19,6 +19,8 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -66,4 +68,19 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+// Test fails if any errors occured while parsing
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
