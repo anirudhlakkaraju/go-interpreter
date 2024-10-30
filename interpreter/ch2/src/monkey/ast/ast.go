f4af67b7f81ea07b475168d306bc376dbfbcb3e2
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-10-29 20:33:23 -0500

    feat: add prefixExpression ast

--- interpreter/ch2/src/monkey/ast/ast.go
+++ interpreter/ch2/src/monkey/ast/ast.go
@@ -133,3 +133,24 @@ type IntegerLiteral struct {
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+// PrefixExpression is both a Node and a Statement
+type PrefixExpression struct {
+	Token    token.Token
+	Right    Expression
+	Operator string
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	// Delilberately adding parenthesis to see which operator belongs to which expression
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
