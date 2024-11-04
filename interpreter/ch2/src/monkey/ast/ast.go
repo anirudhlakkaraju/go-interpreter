924054bf75c21acd5428cf5f378fca9affc3b081
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-03 18:41:34 -0600

    feat: add boolean expression definition

--- interpreter/ch2/src/monkey/ast/ast.go
+++ interpreter/ch2/src/monkey/ast/ast.go
@@ -177,3 +177,13 @@ func (ie *InfixExpression) String() string {
 
 	return out.String()
 }
+
+// Boolean is both a Node and an Expression
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
