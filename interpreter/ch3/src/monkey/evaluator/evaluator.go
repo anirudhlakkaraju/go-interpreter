cccd914121a39737b276227658fc440de586b2ff
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-10 22:22:59 -0600

    feat: evaluate fn calls using enclosed envs

--- interpreter/ch3/src/monkey/evaluator/evaluator.go
+++ interpreter/ch3/src/monkey/evaluator/evaluator.go
@@ -75,6 +75,18 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		params := node.Parameters
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
+
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
+		return applyFunction(function, args)
 	}
 
 	return nil
@@ -268,4 +280,53 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 	}
 
 	return val
+}
+
+// evalExpressions evaluates a slice of Expressions nodes into a slice of Objects
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+
+	return result
+}
+
+// applyFunction executes the function with the given arguments
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	// execute fn body using the extended env
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+// extendFunctionEnv returns an env that's extended with the function arguments
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	// bind the arg values to the fn params
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
 }
--- interpreter/ch3/src/monkey/object/environment.go
+++ interpreter/ch3/src/monkey/object/environment.go
@@ -1,19 +1,34 @@
 package object
 
+// NewEnvironment creates a new environment
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
+// NewEnclosedEnvironment creates a new environment that extends a given outer environment
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
+// Environment holds variable bindings in the current and outer scopes
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
+// Get returns the object bindings from current or outer scope
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
+// Set stores a binding
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
