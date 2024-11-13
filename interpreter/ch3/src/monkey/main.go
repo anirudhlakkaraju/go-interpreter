0c3e76535a1700e467cb9ad5afb031e754e1157d
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-12 21:29:42 -0600

    style: welcome message

--- interpreter/ch3/src/monkey/main.go
+++ interpreter/ch3/src/monkey/main.go
@@ -15,8 +15,7 @@ func main() {
 	}
 
 	fmt.Printf("\nHello %s! This is the Monkey Programming Language!\n", user.Username)
-	fmt.Println("This REPL parses your input into an AST, adding brackets to show precedence!")
-	fmt.Println("Inputs need a semicolon to end; press Ctrl-C to quit. For syntax details, visit: https://monkeylang.org/")
+	fmt.Println("Inputs need a semicolon to end; press Ctrl-C to quit. Synatx: https://monkeylang.org")
 	fmt.Printf("\n")
 
 	repl.Start(os.Stdin, os.Stdout)
