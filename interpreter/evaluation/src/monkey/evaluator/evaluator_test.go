6108577de8e7d8f55f99ef827aa604402c0c3762
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-14 18:04:24 -0600

    test: string concatenation

--- interpreter/evaluation/src/monkey/evaluator/evaluator_test.go
+++ interpreter/evaluation/src/monkey/evaluator/evaluator_test.go
@@ -173,6 +173,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unkown operator: STRING - STRING",
+		},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -264,6 +268,20 @@ func TestStringLiteral(t *testing.T) {
 	str, ok := evaluated.(*object.String)
 	if !ok {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T", evaluated)
 	}
 
 	if str.Value != "Hello World!" {
