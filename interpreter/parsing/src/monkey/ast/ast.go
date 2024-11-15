6b36611b150c685fca1f80df2761a3375fc900ce
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-14 20:20:50 -0600

    feat: define an index expression to index into arrays

--- interpreter/parsing/src/monkey/ast/ast.go
+++ interpreter/parsing/src/monkey/ast/ast.go
@@ -316,3 +316,24 @@ func (al *ArrayLiteral) String() string {
 
 	return out.String()
 }
+
+// IndexExpression is an Expression Node for indexing into Arrays
+type IndexExpression struct {
+	Token token.Token // the `[` token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode()      {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
+
+	return out.String()
+}
