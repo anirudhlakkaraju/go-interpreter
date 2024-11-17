9f111b4b4a80b584bb381882970446a4cb6d387f
anirudhlakkaraju <lakanirudh123@gmail.com>
2024-11-16 18:13:11 -0600

    feat: add puts builtin fn

--- interpreter/evaluation/src/monkey/evaluator/builtins.go
+++ interpreter/evaluation/src/monkey/evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/anirudhlakkaraju/go-interpreter/interpreter/evaluation/src/monkey/object"
+import (
+	"fmt"
+
+	"github.com/anirudhlakkaraju/go-interpreter/interpreter/evaluation/src/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -17,6 +21,14 @@ var builtins = map[string]*object.Builtin{
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 	"first": &object.Builtin{
